fe8610af5080149bded371e901069b614c4a968f
zneref <zneref@gmail.com>
2018-03-03 19:56:52 +0100

    gradle directory included to repo

--- kodilla-hibernate/src/main/java/com/kodilla/zneref/hibernate/manytomany/dao/EmployeeDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/zneref/hibernate/manytomany/dao/EmployeeDao.java
@@ -1,8 +1,6 @@
 package com.kodilla.zneref.hibernate.manytomany.dao;
 
-import com.kodilla.zneref.hibernate.manytomany.Company;
 import com.kodilla.zneref.hibernate.manytomany.Employee;
-import com.kodilla.zneref.hibernate.task.Task;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
